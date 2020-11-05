1989cb5fcb68b1d8a5bfcebed214fd18e85c8aec
DeFalk <hfschoonewille@gmail.com>
2020-11-05 16:13:40 +0100

    Added commentary to the gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,7 @@
+/*
+Author: HF Schoonewille
+*/
+
 import { src, dest, watch, series } from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
@@ -12,6 +16,9 @@ import babel from 'gulp-babel';
 import concat from 'gulp-concat';
 const PRODUCTION = yargs.argv.prod;
 
+/* 
+Create bundle.css and admin.css from all SCSS files imported in bundle.scss and admin.scss
+*/ 
 export const styles = () => {
   return src(['src/scss/bundle.scss', 'src/scss/admin/admin.scss'])
     .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
@@ -22,17 +29,28 @@ export const styles = () => {
     .pipe(dest('public/css'));
 }
 
+/* 
+Process image into ./public/images
+*/ 
 export const images = () => {
   return src('src/images/**/*.{jpg,jpeg,png,svg,gif}')
     .pipe(gulpif(PRODUCTION, imagemin()))
     .pipe(dest('public/images'));
 }
 
+/* 
+Copy all non image, scss and js files from ./src into its own folder inside ./public
+*/ 
 export const copy = () => {
   return src(['src/**/*','!src/{images,js,scss}','!src/{images,js,scss}/**/*'])
     .pipe(dest('public'));
 }
 
+// JS concatination
+/* 
+Concatenate our 3rd party dependencies
+into a temporary main.deps.js file.
+*/ 
 export const jsDeps = () => {
   const files = [
     'src/vendor/jquery.js',
@@ -43,6 +61,10 @@ export const jsDeps = () => {
     .pipe(dest('./tmp'));
 }
 
+/* 
+Concatenate our homegrown components 
+into a temporary main.build.js file.
+*/ 
 export const jsBuild = () => {
   return src('src/components/**/*.js')
     .pipe(plumber())
@@ -57,6 +79,10 @@ export const jsBuild = () => {
     .pipe(dest('./tmp'));
 }
 
+/* 
+Concatenate the third-party libraries and our
+homegrown components into a single app.js file.
+*/ 
 export const jsConcat = () => {
   const files = [
     './tmp/main.deps.js',
@@ -64,16 +90,19 @@ export const jsConcat = () => {
   ];
   return src(files)
     .pipe(plumber())
-    // Concatenate the third-party libraries and our
-    // homegrown components into a single main.js file.
     .pipe(concat('app.js'))
-    // Save it to the final destination.
     .pipe(dest('./public/js'))
 }
+// End JS concatination
+
 
 export const watcher = () => {
+  // Watch source scss files for changes to process
   watch('src/scss/**/*.scss', styles);
+  // Watch source images for changes to process
   watch('src/images/**/*.{jpg,jpeg,png,svg,gif}', images);
+  // Watch every other file we place in ./src and trigger the copy task
   watch(['src/**/*','!src/{images,js,scss}','!src/{images,js,scss,components}/**/*'], copy);
+  // Watch JS and process in the following order: jsDeps > jsBuild > jsConcat 
   watch('src/components/**/*.js', series(jsDeps, jsBuild, jsConcat));
 }
