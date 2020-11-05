2b32144f27170b10eedfcafba2cd69ee5d41bfd3
DeFalk <hfschoonewille@gmail.com>
2020-11-05 14:57:30 +0100

    Gulp now concatinates JS dependencies correctly

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-import { src, dest, watch } from 'gulp';
+import { src, dest, watch, series } from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
@@ -33,10 +33,20 @@ export const copy = () => {
     .pipe(dest('public'));
 }
 
-export const js = () => {
-  return src('src/js/**/*.js')
+export const jsDeps = () => {
+  const files = [
+    'src/vendor/jquery.js',
+    'src/vendor/bootstrap.js'
+  ];
+  return src(files)
+    .pipe(concat('main.deps.js'))
+    .pipe(dest('./tmp'));
+}
+
+export const jsBuild = () => {
+  return src('src/components/**/*.js')
     .pipe(plumber())
-    .pipe(concat('app.js'))
+    .pipe(concat('main.build.js'))
     .pipe(babel({
       presets: [
         ['@babel/env', {
@@ -44,12 +54,26 @@ export const js = () => {
         }]
       ]
     }))
-    .pipe(dest('public/js'));
+    .pipe(dest('./tmp'));
+}
+
+export const jsConcat = () => {
+  const files = [
+    './tmp/main.deps.js',
+    './tmp/main.build.js'
+  ];
+  return src(files)
+    .pipe(plumber())
+    // Concatenate the third-party libraries and our
+    // homegrown components into a single main.js file.
+    .pipe(concat('app.js'))
+    // Save it to the final destination.
+    .pipe(dest('./public/js'))
 }
 
 export const watcher = () => {
   watch('src/scss/**/*.scss', styles);
   watch('src/images/**/*.{jpg,jpeg,png,svg,gif}', images);
-  watch(['src/**/*','!src/{images,js,scss}','!src/{images,js,scss}/**/*'], copy);
-  watch('src/js/**/*.js', js);
+  watch(['src/**/*','!src/{images,js,scss}','!src/{images,js,scss,components}/**/*'], copy);
+  watch('src/components/**/*.js', series(jsDeps, jsBuild, jsConcat));
 }
