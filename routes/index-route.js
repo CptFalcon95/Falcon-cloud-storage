0cd40f22509d1c2f9a421a3d77fc60b54d72e9c5
CptFalcon95 <hfschoonewille@gmail.com>
2020-11-16 11:47:05 +0100

    There is always something you missed

--- routes/index-route.js
+++ routes/index-route.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const userController = require('../controllers/user-controller');
 const router = express.Router();
-const indexController = require('../controllers/user-controller');
 
 router.get('/', userController.index);
 router.get('/register', userController.registerForm);
